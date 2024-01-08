fc9c02d6b3052061448ff21e5af79854b2ee68dc
gporras <gporras@bancolombia.com.co>
2024-01-07 21:17:40 -0500

    improve signIn process

--- src/main/java/com/app/infra/entrypoint/rest/AuthRouter.java
+++ src/main/java/com/app/infra/entrypoint/rest/AuthRouter.java
@@ -5,14 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class SignUpRouter {
 
     @Bean
-    public RouterFunction<ServerResponse> routes(SignUpHandler handler){
+    public RouterFunction<ServerResponse> routes(AuthHandler handler){
         return route().POST("/api/auth/sign-up", handler::signUp)
                 .POST("/api/auth/sign-in", handler::signIn)
                 .build();
